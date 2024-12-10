d5923751eabd8397424bede41abe3ca69ae7e1ec
Juli-30 <carrenocadena@gmail.com>
2024-12-09 20:41:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,17 @@ const dbconnect = () => {
     .catch((err)=> console.log(err.message));
 }
 
+const dbconnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Conexión exitosa a la base de datos");
+  } catch (error) {
+    console.error("Error al conectar a la base de datos", error);
+    process.exit(1);
+  }
+};
+
 module.exports = dbconnect;
