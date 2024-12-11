b7a02a4c374ca12f2472ab12d1f8b72089419d1f
Karen Juliana Carreño Cadena <carrenocadena@gmail.com>
2024-12-11 17:40:18 -0500

    backend2

--- routes/empleado.routes.js
+++ routes/empleado.routes.js
@@ -8,4 +8,17 @@ router.get('/:id', empleadoCtrl.getUniqueEmpleado);
 router.put('/:id', empleadoCtrl.editEmpleado);
 router.delete('/:id', empleadoCtrl.deleteEmpleado);
 
+router.get('/', async (req, res) => {
+    console.log("Inicio de la consulta de empleados");
+    try {
+      const empleados = await Empleado.find(); // Si usas Mongoose
+      console.log("Consulta exitosa:", empleados);
+      res.status(200).json(empleados);
+    } catch (error) {
+      console.error("Error en la consulta:", error);
+      res.status(500).json({ error: "Error al obtener empleados" });
+    }
+  });
+  
+
 module.exports = router;
